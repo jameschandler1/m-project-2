4976ca053f22cac077351ec4efe254b7f3ac96da
james chandler <80052602+jameschandler1@users.noreply.github.com>
2026-06-11 14:53:07 -0700

    add files associated with terrafrom to be ignored to .gitignore, and ran terraform init which created .terraform/ and .terraform.lock.hcl

---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS deployment region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "didyoudoityet"
+}
