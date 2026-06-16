64f487a0f3800025a0fbfe643c0bdceda78fcb73
James C <jamesc@Jamess-MacBook-Pro.local>
2026-06-15 21:02:34 -0700

    create HTMX frontend, now testing payment and feature functionality

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -10,6 +10,17 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 4000
       }
+    },
+    {
+      name: "didyoudoityet-htmx",
+      script: "server.js",
+      cwd: process.env.APP_DIR || "/opt/didyoudoityet/frontend-htmx",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
     }
   ]
 };
